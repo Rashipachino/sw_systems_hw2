809afbfb5d31b247303473cccb1f7fb4d229424b
noahweiss890 <nweiss890@gmail.com>
2021-11-23 01:38:08 +0200

    noah third

--- my_mat.c
+++ my_mat.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include "my_mat.h"
+
+#define N 10
+
+void optionA(int mat[N][N]) {
+    for(int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+void optionB(int mat[N][N]) {
+
+}
+
+void optionC(int mat[N][N]) {
+
+}
--- my_mat.h
+++ my_mat.h
@@ -1,7 +1,8 @@
 #ifndef MY_MAT_H
     #define MY_MAT_H
+    #define N 10
 
-    void optionA();
-    void optionB(int);
-    void optionC(int);
+    void optionA(int a[N][N]);
+    void optionB(int mat[N][N]);
+    void optionC(int mat[N][N]);
 #endif
