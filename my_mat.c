cba25e6b804bcc21363459afc242fa84f46a3ff6
noahweiss890 <nweiss890@gmail.com>
2021-11-23 18:20:28 +0200

    fifth

--- my_mat.c
+++ my_mat.c
@@ -3,6 +3,12 @@
 
 #define N 10
 
+int main() {
+    int arr[N][N];
+    optionA(arr);
+    optionB(arr);
+}
+
 void optionA(int mat[N][N]) {
     for(int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -14,7 +20,8 @@ void optionA(int mat[N][N]) {
 void optionB(int mat[N][N]) {
     int a, b;
     scanf("%d %d", &a, &b);
-    if(optionBHelper(mat, a, b,)) {
+    int been_there[N] = {0};
+    if(optionBHelper(mat, a, b, been_there)) {
         printf("True");
     }
     else {
@@ -22,15 +29,17 @@ void optionB(int mat[N][N]) {
     }
 }
 
-int optionBHelper(int mat[N][N], int a, int b) {  // not done
+int optionBHelper(int mat[N][N], int a, int b, int been_there[N]) {
     if(mat[a][b] != 0) {
         return 1;
     }
+    been_there[a] = 1;
     for (int i = 0; i < N; i++) {
-        if(mat[a][i] && optionBHelper(mat, i, b)) {
+        if(mat[a][i] && been_there[i] == 0 && optionBHelper(mat, i, b, been_there)) {
             return 1;
         }
     }
+    return 0;
 }
 
 void optionC(int mat[N][N]) { // not done
