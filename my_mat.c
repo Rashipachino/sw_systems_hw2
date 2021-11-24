e0f8ff42cc17fcdf06ccc2ac5207537b5bce9d67
noahweiss890 <nweiss890@gmail.com>
2021-11-24 20:31:52 +0200

    eighth

--- my_mat.c
+++ my_mat.c
@@ -6,53 +6,50 @@
 
 #define N 10
 
-void optionA(int mat[N][N]) {
+void optionA(int mat[N][N]) {  // fills the matrix with the inputted ints
     for(int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            scanf("%d", &mat[i][j]);
+            scanf("%d", &mat[i][j]);  // input the next int
         }
     }
 }
 
-int optionB(int mat[N][N], int a, int b) {
+int optionB(int mat[N][N], int a, int b) {  // returns 1 if there is a path between a and b otherwise returns 0
     if(a == b) {
         return 0;
     }
-    int been_there[N] = {0};
+    int been_there[N] = {0};  // to keep track of the nodes that i have already been to
     return optionBHelper(mat, a, b, been_there);
 }
 
-int optionBHelper(int mat[N][N], int a, int b, int been_there[N]) {
-    if(mat[a][b] != 0) {
+int optionBHelper(int mat[N][N], int a, int b, int been_there[N]) {  // recursive function that travels through the paths that start from node a and returns 1 if it gets to node b otherwise returns 0
+    if(mat[a][b] != 0) {  // if there is a path between a and b
         return 1;
     }
-    been_there[a] = 1;
+    been_there[a] = 1; // update been_there that ive been to this node
     for (int i = 0; i < N; i++) {
-        if(mat[a][i] && been_there[i] == 0) {
-            return optionBHelper(mat, i, b, been_there);
+        if(mat[a][i] && been_there[i] == 0) {  // if there is a path and if I haven't been to that node yet
+            return optionBHelper(mat, i, b, been_there);  // recursively calls itself as the node i
         }
     }
-    return 0;
+    return 0; // if there is no path to b
 }
 
-int optionC(int mat[N][N], int a, int b) {
-    if(a == b) {
-        return -1;
-    }
-    int A[N][N] = {0};
-    for (int i = 0; i < N; i++) {
+void optionC(int mat[N][N], int shortest_paths[N][N]) {  // this function is based off the Floyd–Warshall algorithm. it crates a new matrix that will hold all of the values of the shortest path between two nodes and then copies it to shortest_paths
+    int A[N][N] = {0};  // creates a matrix that will keep track of the values of the shortest path from one node to another
+    for (int i = 0; i < N; i++) {  // these fors are for copying the original matrix to a new one with modifications so that it will work with the Floyd–Warshall algorithm
         for (int j = 0; j < N; j++) {
             if(i != j) {
-                if(mat[i][j] == 0) {
+                if(mat[i][j] == 0) {  // if the value is 0 then there is no path between the two nodes so set it to be -1 in A
                     A[i][j] = -1;
                 }
                 else {
-                    A[i][j] = mat[i][j];
+                    A[i][j] = mat[i][j];  // otherwise copy the value
                 }
             }
         }
     }
-    for (int k = 0; k < N; k++) {
+    for (int k = 0; k < N; k++) {  // implementation of the Floyd–Warshall algorithm
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if(i != j && i != k && j != k) {
@@ -61,18 +58,14 @@ int optionC(int mat[N][N], int a, int b) {
             }
         }
     }
-//    printf("\n\nC (%d, %d)\n", a, b);
-//    for (int i = 0; i < N; i++) {
-//        printf("%d  ", i);
-//        for (int j = 0; j < N; j++) {
-//            printf("%d ", A[i][j]);
-//        }
-//        printf("\n");
-//    }
-    return A[a][b];
+    for (int i = 0; i < N; i++) { // copies the values of A in to shortest_paths which will be used in the main function
+        for (int j = 0; j < N; j++) {
+            shortest_paths[i][j] = A[i][j];
+        }
+    }
 }
 
-int min(int a, int b, int c) {
+int min(int a, int b, int c) {  // checks what the minimum of a and b+c
     if(b == -1 || c == -1) {
         if(a == -1) {
             return -1;
